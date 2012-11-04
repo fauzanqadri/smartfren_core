4b764f71039202ce4c4ff63cb027f3700e99cf2d
Fauzan Qadri <ojankill@gmail.com>
2012-11-05 02:06:23 +0700

    More Human readable

--- lib/smartfren_core/base/smartfren.rb
+++ lib/smartfren_core/base/smartfren.rb
@@ -4,19 +4,68 @@ module SmartfrenCore
     class Smartfren < Bridge
       
       def reload voucher_code
+        login
+        data_before = featchInfo
         page = @agent.post("http://data.smartfren.com/index.php/buy_package_connex/index",{:voucher_code => voucher_code, :reload => "Reload"},{"Referer" =>  "http://data.smartfren.com/index.php/user_info","Content-Type" => "application/x-www-form-urlencoded"})
-        puts page.body
+        data_after = featchInfo
+        if data_after[:balance] > data_before[:balance]
+          puts "Balance Before Reload #{data_before[:balance]} and now your balance is #{data_after[:balance]}"
+          logout
+          true
+        else
+          puts "Hey Something Wrong With your voucher code, yeah.. i'm sure that"
+          logout
+          false
+        end
       end
       
-      def buy
-        premium = "http://data.smartfren.com/index.php/buy_package_connex/buy/1"
-        reguler = "http://data.smartfren.com/index.php/buy_package_connex/buy/19"
+      def buy paket
+        login
+        num = 1 if paket == 'daily'
+        num = 2 if paket == 'weekly'
+        num = 3 if paket == 'monthly'
+        num = 19 if paket == 'reguler'
+        data_before = featchInfo
+        page = @agent.get(paketUrl(num))
+        data_after = featchInfo
+        if data_before[:packageExpiry] != data_after[:packageExpiry]
+          logout
+          puts "Ok, Your Package Expiry is #{data_after[:packageExpiry]}"
+          return true
+        else
+          logout
+          puts "emmm.... lets check... #{data_after[:packageExpiry]}... Something error with me"
+          return false
+        end
         
       end
       
       def info
+        login
+        data = featchInfo
+        logout
+        data
+      end
+      
+     
+      
+      private
+      
+      def featchInfo
         page = @agent.get("http://data.smartfren.com/index.php/user_info")
-        puts page.body
+        infoRes = Nokogiri::HTML(page.body)
+        infoTable = infoRes.css("div#header div#content-details div#content-details-inside-left span.fonthitam table")
+        phoneNumber = infoTable[0].css('tr:nth-child(1) td:nth-child(2)').text.gsub("\t",'')[1..-1]
+        balance = infoTable[0].css('tr:nth-child(3) td:nth-child(2)').text.gsub("\t",'')[1..-1].to_i
+        typeDataPackage = infoTable[1].css('tr:nth-child(1) td:nth-child(2)').text.gsub("\t",'')[1..-1]
+        packageExpiry = infoTable[1].css('tr:nth-child(2) td:nth-child(2)').text.gsub("\t",'')[1..-1]
+        data = {
+          :phoneNumber      => phoneNumber,
+          :balance          => balance,
+          :typeDataPackage  => typeDataPackage,
+          :packageExpiry    => packageExpiry
+        }
+        data
       end
       
       def login
@@ -31,6 +80,10 @@ module SmartfrenCore
       def logout
         @agent.get "http://data.smartfren.com/index.php/main/logout"
         true
+      end
+      
+      def paketUrl num
+        "http://data.smartfren.com/index.php/buy_package_connex/buy/#{num.to_s}"
       end
       
     end
